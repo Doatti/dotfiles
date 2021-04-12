3eab4b68a283e68bb2bf196954654f62e3ec5f90
Tatu Rajaniemi <tatu.rajaniemi@gmail.com>
2021-04-12 18:17:07 +0300

    Add emacs and bash stuff

---
+++ emacs/init.el
@@ -0,0 +1,58 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(add-to-list 'load-path "~/.emacs.d/lisp")
+
+(require 'cp2k-mode nil 'noerror)
+(add-to-list 'auto-mode-alist '("\\.inp\\'" . cp2k-mode))
+
+;; Disable tool bar, menu bar, scroll bar.
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; Better defaults
+
+(setq-default
+ ;; Use colors that fit a dark background.
+ frame-background-mode 'dark
+
+ ;; Don't use the compiled code if its the older package.
+ load-prefer-newer t
+
+ ;; Do not show the startup message.
+ inhibit-startup-message t
+
+ ;; Do not put 'customize' config in init.el; give it another file.
+ custom-file "~/.emacs.d/custom-file.el"
+
+ ;; 72 is too less for the fontsize that I use.
+ fill-column 90
+
+ ;; Use your name in the frame title. :)
+ frame-title-format (format "%s's Emacs" (capitalize user-login-name))
+
+ ;; Do not create lockfiles.
+ ;create-lockfiles nil
+
+ ;; Don't use hard tabs
+ indent-tabs-mode nil
+
+ ;; Emacs can automatically create backup files. This tells Emacs to put all backups in
+ ;; ~/.emacs.d/backups. More info:
+ ;; http://www.gnu.org/software/emacs/manual/html_node/elisp/Backup-Files.html
+ backup-directory-alist `(("." . ,(concat user-emacs-directory "backups")))
+
+ ;; Do not autosave.
+ auto-save-default nil
+
+ ;; Allow commands to be run on minibuffers.
+ enable-recursive-minibuffers t)
+
+;; Load customization file
+(load custom-file)
